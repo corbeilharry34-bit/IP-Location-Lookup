a1331feaef27ec6aca5d971149f8fffd33e2da8b
corbeilharry34-bit <corbeilharry34@gmail.com>
2026-09-09 17:53:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const { Telegraf } = require('telegraf');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const MINI_APP_URL = process.env.MINI_APP_URL;
+
 bot.start((ctx) => {
   ctx.reply(
     'Check your IP address and location, or look up any IP/domain — right here in Telegram.',
     {
       reply_markup: {
         inline_keyboard: [[
-          { text: '🌍 Open IP Lookup',
+          { text: '🌍 Open IP Lookup', web_app: { url: https://serene-gelato-7538ee.netlify.app/} }
         ]]
       }
     }
@@ -20,7 +22,7 @@ bot.command('lookup', (ctx) => {
   ctx.reply('Tap below to open the tool:', {
     reply_markup: {
       inline_keyboard: [[
-      }
+        { text: '🌍 Open IP Lookup', web_app: { url: https://serene-gelato-7538ee.netlify.app/ } }
       ]]
     }
   });
